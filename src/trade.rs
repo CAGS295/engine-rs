6a587cfb27c9dc96f945d01bd03627208024ffdd
Adrian Stobbe <stobbe.adrian@gmail.com>
2022-10-01 11:50:59 +0200

    add sell

--- src/trade.rs
+++ src/trade.rs
@@ -10,7 +10,6 @@ async fn buy(
   price: f64,
 ) -> Result<Transaction, binance::errors::Error> {
   let account: Account = Binance::new_with_env(&Config::testnet());
-  let symbol = symbol;
   let market_buy = OrderRequest {
     symbol: symbol.to_string(),
     quantity: Some(quantity),
@@ -22,6 +21,25 @@ async fn buy(
   };
 
   account.place_order(market_buy).await
+}
+
+async fn sell(
+  symbol: &str,
+  quantity: f64,
+  price: f64,
+) -> Result<Transaction, binance::errors::Error> {
+  let account: Account = Binance::new_with_env(&Config::testnet());
+  let market_buy = OrderRequest {
+    symbol: symbol.to_string(),
+    quantity: Some(quantity),
+    price: Some(price),
+    order_type: OrderType::Limit,
+    time_in_force: Some(TimeInForce::FOK),
+    side: OrderSide::Sell,
+    ..OrderRequest::default()
+  };
+  let res = account.place_order(market_buy).await;
+  return res;
 }
 
 #[cfg(test)]
@@ -37,6 +55,18 @@ mod test {
     let quantity = 0.001;
     let price = 19000.;
     let result = buy(symbol, quantity, price).await;
+    println!("{:?}", result);
+    assert!(result.is_ok());
+  }
+
+  #[actix_rt::test]
+  async fn test_sell() {
+    dotenv().ok();
+
+    let symbol = "BTCUSDT";
+    let quantity = 0.001;
+    let price = 19000.;
+    let result = sell(&symbol, quantity, price).await;
     println!("{:?}", result);
     assert!(result.is_ok());
   }
