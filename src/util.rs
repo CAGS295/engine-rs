7459379b4bd6ab0a7c12c5cb9ab828a2e648291e
Adrian Stobbe <stobbe.adrian@gmail.com>
2022-10-01 16:47:05 +0200

    stream ticker msg

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,5 @@
 use actix::Message;
+use serde::{de::Error, Deserialize};
 
 #[derive(Message)]
 #[rtype(result = "f64")]
@@ -11,3 +12,15 @@ pub struct Return(pub f64);
 #[derive(Message)]
 #[rtype(result = "Option<f64>")]
 pub struct SharpeRatio(pub f64);
+
+pub fn deserialize_from_str<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+where
+  D: serde::de::Deserializer<'de>,
+  T: std::str::FromStr,
+{
+  let s = String::deserialize(deserializer)?;
+
+  s.parse().map_err(|_| {
+    Error::custom("failed to parse deserialized value to desired type")
+  })
+}
