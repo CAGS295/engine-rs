3b38f817bd465334e5fc50944943d04694022a36
rcheng <robinson.cheng@zalando.de>
2022-10-01 03:04:45 +0200

    Add function for reading web socket stream

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,73 @@
-use actix_web::{web, App, HttpServer};
-use engine_rs::test_server::*;
+use std::{io, thread};
+
+use actix_web::web::Bytes;
+use awc::{ClientBuilder, ws};
+use futures_util::{SinkExt as _, StreamExt as _};
+use tokio::{select, sync::mpsc};
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use openssl::ssl::{SslConnector, SslMethod};
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-        .bind(("127.0.0.1", 8080))?
-        .run()
+async fn main() {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting echo WebSocket client");
+
+    let (cmd_tx, cmd_rx) = mpsc::unbounded_channel();
+    let mut cmd_rx = UnboundedReceiverStream::new(cmd_rx);
+
+    // run blocking terminal input reader on separate thread
+    let input_thread = thread::spawn(move || loop {
+        let mut cmd = String::with_capacity(32);
+
+        if io::stdin().read_line(&mut cmd).is_err() {
+            log::error!("error reading line");
+            return;
+        }
+
+        cmd_tx.send(cmd).unwrap();
+    });
+    let ssl_connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
+    let awc_connector = awc::Connector::new().openssl(ssl_connector);
+    let ssl_client = awc::Client::builder().connector(awc_connector).finish();
+    let (res, mut ws) = ssl_client
+        .ws("wss://stream.binance.com:9443/ws/btcusdt@depth5")
+        .connect()
         .await
+        .unwrap();
+
+    log::debug!("response: {res:?}");
+    log::info!("connected; server will echo messages sent");
+
+    loop {
+        select! {
+            Some(msg) = ws.next() => {
+                match msg {
+                    Ok(ws::Frame::Text(txt)) => {
+                        // log echoed messages from server
+                        log::info!("Server: {txt:?}")
+                    }
+
+                    Ok(ws::Frame::Ping(_)) => {
+                        // respond to ping probes
+                        ws.send(ws::Message::Pong(Bytes::new())).await.unwrap();
+                    }
+
+                    _ => {}
+                }
+            }
+
+            Some(cmd) = cmd_rx.next() => {
+                if cmd.is_empty() {
+                    continue;
+                }
+
+                ws.send(ws::Message::Text(cmd.into())).await.unwrap();
+            }
+
+            else => break
+        }
+    }
+
+    input_thread.join().unwrap();
 }
